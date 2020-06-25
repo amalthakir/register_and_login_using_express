af6a5d0d10f47646e7f847919c897b6bf3398388
Amal Alotibi <amal.m.thakir@gmail.com>
2020-06-25 14:22:11 +0300

    fainal

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose= require("mongoose");
 require("dotenv/config");
 const routerPost =require("./routes/register_and_login");
 const BodyParser =require("body-parser");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
@@ -19,8 +20,6 @@ app.use("/user",routerPost);
 app.get('/',(req,res)=>{
     res.send("hi amal you are in the home ");
 });
-
-
 
 const PORT = process.env.PORT||5000;
 app.listen(PORT);
--- routes/register_and_login.js
+++ routes/register_and_login.js
@@ -2,36 +2,75 @@ const express = require("express");
 const router = express.Router();
 const User= require("../modules/UserModules");
 const uuid =require("uuid");
+const core =require("cors");
+const jwk =require("jsonwebtoken");
+router.use(core());
 
-router.get('/', async (req,res) => {
-    const users= await User.find();
-    res.json(users);
-    //res.send("hi amal you are in post");
-});
+process.env.SECRET_KEY="secret";
 
 router.post('/register',(req,res)=>{
 
-    let user = new  User({
+    let UserData = {
         name: req.body.name,
         username: req.body.username,
         password: req.body.password,
         email: req.body.email
-    });
-   
-    console.log(req.body);
+    }
 
-    user.save()
-    .then(data =>{
-        res.json(data)
+    User.findOne({
+        email: req.body.email 
     })
-    .catch(err=>{
-        res.json({mass:err});
+    .then(found => {
+        if(!found){
+            User.create(UserData)
+            .then(data =>{
+                res.json(data);
+            })
+            .catch(err =>{
+                res.send(err);
+            })
+        }
+        else{
+            res.json({ error : "user already existed" });
+        }
+    })
+    .catch(err =>{
+        res.send(err);
     });
+})
 
-    //console.log(req.body);
+router.post('/login',(req,res)=>{
 
-});
+    User.findOne({
+        email: req.body.email 
+    })
+    .then(found =>{
+        if(found){
+            if(req.body.password === found.password){
+                let payload = {
+                    _id: found._id,
+                    name: found.name,
+                    username: found.username,
+                    password: found.password,
+                    email: found.email
+                }  
 
-
+                let token = jwk.sign(payload, process.env.SECRET_KEY,{
+                    expiresIn: 1440
+                })
+                res.send(token);
+            }
+            else{
+                res.json({ error : "Check your email and password" });
+            }
+        }
+        else{
+            res.json({ error : "Check your email and password" });
+        }
+    })
+    .catch(err =>{
+        res.send(err);
+    });
+})
 
 module.exports=router;
